9d1ccb01e4563d6e2d67ab01e4de0972d918601a
Ida Darmochwał <ida.mdart@gmail.com>
2024-01-14 15:51:29 +0100

    Add login, logout, registration features

--- website/views.py
+++ website/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+  if request.method == "POST":
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+      login(request, user)
+      messages.success(request, "Logged in successfully", extra_tags='success')
+      return redirect('home')
+    else:
+      messages.error(request, "There was an error logging in. Please try again", extra_tags='danger')
+      return redirect('home')
+  else:
+    return render(request, 'home.html', {})
+
+
+def logout_user(request):
+  logout(request)
+
+  # if request.user.is_authenticated:
+  #   messages.error(request, "There was an error logging out. Please try again", extra_tags='danger')
+  #   return redirect('home')
+  # else:
+  messages.success(request, "Logged out successfully", extra_tags='success')
+  return redirect('home')
+
+
+def register_user(request):
+  return render(request, 'register.html', {})
